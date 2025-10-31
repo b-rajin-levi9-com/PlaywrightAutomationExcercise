85bb625beb96386cde26386ad59c579a67d8c01e
b.rajin <b.rajin@levi9.com>
2025-10-31 14:33:57 +0100

    updated number of workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? undefined: 4,
+  workers: process.env.CI ? 4: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['allure-playwright']],
   timeout: 30000,
